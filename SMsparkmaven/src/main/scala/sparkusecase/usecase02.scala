f1d8112d9493cb65315fb8bf8530df0de0cf26ad
priya <lakshmi.pri@gmail.com>
2021-06-22 09:03:21 +0000

    Added usecase02 in spark usecase

---
+++ SMsparkmaven/src/main/scala/sparkusecase/usecase02.scala
@@ -0,0 +1,28 @@
+package sparkusecase
+
+import org.apache.spark.sql.SparkSession
+
+
+object usecase02 {
+  
+  def main(args:Array[String])=
+  {
+    
+    val spark = SparkSession.builder().appName("usecase02-SQL")
+    .config("spark.sql.warehouse.dir","file:/tmp/warehouse")
+    .master("local").enableHiveSupport().getOrCreate()
+     
+    spark.sparkContext.setLogLevel("ERROR")
+    
+     val df = spark.read.format("jdbc")
+    .option("url","jdbc:mysql://localhost/custdb")
+    .option("user","root")
+    .option("password","Root123$")
+    .option("dbtable","tblorderdata")
+    .option("driver","com.mysql.cj.jdbc.Driver").load()
+  
+    df.write.mode("append").saveAsTable("tblorderhive1")
+    
+  }
+  
+}
